42289a6e532aec9b856ab5120c3ccdac660924e1
franiroh <franiroh@users.noreply.github.com>
2026-05-14 23:36:25 -0300

    feat: add responsive Footer component with contact information and social links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,34 +12,35 @@ export default function Footer({ content }) {
           <img src="/images/logo-blanco.png" alt="CAdBA Logo Blanco" className={styles.logo} />
         </div>
         
-        {/* Contact Info 1: Location */}
-        <div className={styles.infoBlock}>
-          <MapPin size={24} className={styles.icon} />
-          <div className={styles.textStack}>
-            <span className={styles.textBold}>{content.address_line1}</span>
-            <span className={styles.textNormal}>{content.address_line2}</span>
+        {/* Contact Info Column: Location & Phone */}
+        <div className={styles.infoColumn}>
+          {/* Location */}
+          <div className={styles.infoBlock}>
+            <MapPin size={24} className={styles.icon} />
+            <div className={styles.textStack}>
+              <span className={styles.textBold}>{content.address_line1}</span>
+              <span className={styles.textNormal}>{content.address_line2}</span>
+            </div>
+          </div>
+          
+          {/* Phone & Hours */}
+          <div className={styles.infoBlock}>
+            <Phone size={24} className={styles.icon} />
+            <div className={styles.textStack}>
+              <span className={styles.textBold}>{content.phone}</span>
+              <span className={styles.textNormal}>{content.hours}</span>
+            </div>
           </div>
         </div>
         
-        {/* Contact Info 2: Instagram */}
-        <div className={styles.infoBlockCenter}>
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={styles.icon}>
+        {/* Social / Instagram */}
+        <div className={styles.infoBlock}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#FFFFFF" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={styles.icon}>
             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"/>
             <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"/>
             <line x1="17.5" x2="17.51" y1="6.5" y2="6.5"/>
           </svg>
-          <span className={styles.textNormal}>{content.instagram}</span>
-        </div>
-        
-        {/* Contact Info 3: Phone & Hours */}
-        <div className={styles.infoStackRight}>
-          <div className={styles.row}>
-            <Phone size={18} className={styles.icon} />
-            <span className={styles.textBold}>{content.phone}</span>
-          </div>
-          <div className={styles.row}>
-            <span className={styles.textNormal} style={{marginLeft: '26px'}}>{content.hours}</span>
-          </div>
+          <span className={styles.textBold}>{content.instagram}</span>
         </div>
       </div>
     </footer>
